155724f50cd227b01ecd6304df6c7d34f2996efc
Leonardo Lima de Vasconcellos <leonardolimadevasconcellos@gmail.com>
2026-01-13 07:58:55 -0300

    posts form with valibot

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { getAllPosts } from './posts.remote';
+
+	//Query for all posts
+	//const posts = await getAllPosts();
+</script>
+
+<!-- List all posts -->
+<ol>
+	{#each await getAllPosts() as post (post.id)}
+		<li>{post.title}</li>
+	{/each}
+</ol>
---
+++ src/routes/admin/post/new/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { createPost } from '../../posts.remote';
+</script>
+
+<form {...createPost}>
+	<div class="row">
+		<label
+			>Title:
+			<input {...createPost.fields.title.as('text')} />
+		</label>
+		{#each createPost.fields.title.issues() as issue (issue.message)}
+			<p>{issue.message}</p>
+		{/each}
+	</div>
+	<div class="row">
+		<label>Body: <textarea {...createPost.fields.body.as('text')}></textarea></label>
+	</div>
+	<button type="submit">Post</button>
+</form>
